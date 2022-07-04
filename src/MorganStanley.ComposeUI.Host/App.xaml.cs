04519da05db2fb1c021db097c505ebddcfc8a2f2
Róbert Zöldi-Kovács <zkovacs.robert@gmail.com>
2022-07-04 23:14:25 +0200

    Initial prototype for WPF host - Basic application lifecycle - Initial communication module integration - Demo with WPF user controls

---
+++ src/MorganStanley.ComposeUI.Host/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace MorganStanley.ComposeUI.Host
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
