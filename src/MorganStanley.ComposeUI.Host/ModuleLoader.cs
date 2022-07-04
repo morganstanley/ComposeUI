47076d6222efb2e19bbd42f616379d549065b7ff
Róbert Zöldi-Kovács <zkovacs.robert@gmail.com>
2022-07-04 23:14:25 +0200

    Initial prototype for WPF host - Basic application lifecycle - Initial communication module integration - Demo with WPF user controls

---
+++ src/MorganStanley.ComposeUI.Host/ModuleLoader.cs
@@ -0,0 +1,59 @@
+﻿/*
+* Morgan Stanley makes this available to you under the Apache License,
+* Version 2.0 (the "License"). You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0.
+*
+* See the NOTICE file distributed with this work for additional information
+* regarding copyright ownership. Unless required by applicable law or agreed
+* to in writing, software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+* or implied. See the License for the specific language governing permissions
+* and limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ComposeUI.Messaging.Client;
+using ComposeUI.Messaging.Client.Transport.WebSocket;
+using MorganStanley.ComposeUI.Host.Modules;
+using MorganStanley.ComposeUI.Interfaces;
+
+namespace MorganStanley.ComposeUI.Host
+{
+    internal class ModuleLoader
+    {
+        private readonly CommunicationModule _communicationModule;
+        private readonly List<IModule> _apps = new List<IModule>();
+
+        public ModuleLoader(IEnumerable<IModule> modules, CommunicationModule communicationModule)
+        {
+            _apps.AddRange(modules);
+            _communicationModule = communicationModule;
+        }
+
+        public async Task LoadModules()
+        {
+            await _communicationModule.Initialize(null);
+            await InitializeApps();
+        }
+
+        private Task InitializeApps()
+        {
+            var tasks = new List<Task>();
+            foreach (var app in _apps)
+            {
+                var client = MessageRouter.Create(
+                     mr => mr.UseWebSocket(
+                         new MessageRouterWebSocketOptions
+                         {
+                             Uri = new Uri("ws://localhost:5000/ws")
+                         }));
+                tasks.Add(app.Initialize(client));
+            }
+
+            return Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ src/MorganStanley.ComposeUI.Prototypes.WebAppHost/WebControl.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace MorganStanley.ComposeUI.Prototypes.WebAppHost
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class WebControl : UserControl
+    {
+        public WebControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
