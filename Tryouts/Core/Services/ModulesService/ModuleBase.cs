62f6a6eaa0106fed8c890761f8e51a803840bc0e
Róbert Zöldi-Kovács <zkovacs.robert@gmail.com>
2022-08-23 18:52:30 +0200

    Moved moduleloader proto code under the skeleton folders

---
+++ Tryouts/Core/Services/ModulesService/ModuleBase.cs
@@ -0,0 +1,34 @@
+﻿/// ********************************************************************************************************
+///
+/// Morgan Stanley makes this available to you under the Apache License, Version 2.0 (the "License").
+/// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+/// See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+/// Unless required by applicable law or agreed to in writing, software distributed under the License
+/// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and limitations under the License.
+/// 
+/// ********************************************************************************************************
+
+using MorganStanley.ComposeUI.Tryouts.Core.Abstractions.Modules;
+using System.Reactive.Subjects;
+
+namespace MorganStanley.ComposeUI.Tryouts.Core.Services.ModulesService;
+
+public abstract class ModuleBase : IModule
+{
+    public ModuleBase(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    protected readonly Subject<LifecycleEvent> _lifecycleEvents = new Subject<LifecycleEvent>();
+    public IObservable<LifecycleEvent> LifecycleEvents => _lifecycleEvents;
+
+    public abstract Task Initialize();
+
+    public abstract Task Launch();
+
+    public abstract Task Teardown();
+}
---
+++ Tryouts/Core/Services/ModulesService/WebpageModule.cs
@@ -0,0 +1,41 @@
+﻿/// ********************************************************************************************************
+///
+/// Morgan Stanley makes this available to you under the Apache License, Version 2.0 (the "License").
+/// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+/// See the NOTICE file distributed with this work for additional information regarding copyright ownership.
+/// Unless required by applicable law or agreed to in writing, software distributed under the License
+/// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and limitations under the License.
+/// 
+/// ********************************************************************************************************
+
+using MorganStanley.ComposeUI.Tryouts.Core.Abstractions.Modules;
+
+namespace MorganStanley.ComposeUI.Tryouts.Core.Services.ModulesService;
+
+internal class WebpageModule : ModuleBase
+{
+    public WebpageModule(string name, string url) : base(name)
+    {
+        _url = url;
+    }
+
+    private string _url;
+
+    public override Task Initialize()
+    {
+        return Task.CompletedTask;
+    }
+
+    public override Task Launch()
+    {
+        _lifecycleEvents.OnNext(LifecycleEvent.Started(new ProcessInfo(Name, UIType.Web, _url)));
+        return Task.CompletedTask;
+    }
+
+    public override Task Teardown()
+    {
+        _lifecycleEvents.OnNext(LifecycleEvent.Stopped(new ProcessInfo(Name, UIType.Web, _url)));
+        return Task.CompletedTask;
+    }
+}
