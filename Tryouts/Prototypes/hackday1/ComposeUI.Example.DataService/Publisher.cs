f0eb4eed9e6ff21369a9eab2ecb3f2d455a515df
Maria Krupl <73655443+kruplm@users.noreply.github.com>
2022-09-05 12:46:24 +0200

    Extracting SubscriptionsMonitor

--- Tryouts/Prototypes/hackday1/ComposeUI.Example.DataService/Publisher.cs
+++ Tryouts/Prototypes/hackday1/ComposeUI.Example.DataService/Publisher.cs
@@ -24,52 +24,6 @@ using Microsoft.Extensions.Logging;
 
 namespace ComposeUI.Example.DataService
 {
-    internal class SubscriptionsMonitor : IObserver<RouterMessage>
-    {
-        public SubscriptionsMonitor(IMessageRouter messageRouter)
-        {
-            _messageRouter = messageRouter;
-        }
-        private readonly List<RouterMessage> _subscriptions = new();
-        public MonthlySalesDataModel? Selected;
-        private readonly IMessageRouter _messageRouter;
-        private readonly ILogger<SubscriptionsMonitor>? _logger;
-
-        public void OnCompleted()
-        {
-            _subscriptions.Clear();
-        }
-
-        public void OnError(Exception error)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async void OnNext(RouterMessage message)
-        {
-            _subscriptions.Add(message);
-
-            var payload = message.Payload;
-   
-            if (payload != null) {
-                Selected = JsonSerializer.Deserialize<MonthlySalesDataModel>(payload, MonthlySalesDataModel.JsonSerializerOptions);
-            
-                if (Selected != null)
-                {
-                    var toPublish = MonthlySalesData.MyList[Selected.Symbol];
-
-                    try
-                    {
-                        await _messageRouter.PublishAsync("proto_select_monthlySales", JsonSerializer.Serialize(toPublish, MonthlySalesDataModel.JsonSerializerOptions));
-                    }
-                    catch (Exception exception)
-                    {
-                        _logger?.LogError(exception.ToString());
-                    }
-                } 
-            }
-        }
-    }
     internal class Publisher
     {
         public static Uri WebsocketURI { get; set; } = new("ws://localhost:5000/ws");
