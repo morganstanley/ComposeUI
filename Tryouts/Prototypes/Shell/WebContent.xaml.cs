65e926a4a3b202e2e476993dfba3f28c3c31c0f9
Maria Krupl <73655443+kruplm@users.noreply.github.com>
2022-10-28 17:51:28 +0200

    Set up layout in xaml

--- Tryouts/Prototypes/Shell/WebContent.xaml.cs
+++ Tryouts/Prototypes/Shell/WebContent.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Web.WebView2.Wpf;
 using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
 
 namespace Shell
 {
@@ -23,42 +24,26 @@ namespace Shell
     public partial class WebContent : Window
     {
         private static string _uriString = "https://www.morganstanley.com";
-        private WebView2 _webView2 = new WebView2 { Name = "WebView2", Source = new Uri(_uriString) };
-
-        private DockPanel _webViewDockPanel = new DockPanel();
-        private DockPanel _navBarDockPanel = new DockPanel();
-        private TextBox _textBox = new TextBox { Name = "addressBar" };
-
+        
         public void SetUpAndShowWindow()
         {
-            DockPanel.SetDock(_navBarDockPanel, Dock.Top);
-            var button = new Button { Name = "ButtonGo", Content = "Go" };
-            button.Click += ButtonGo_Click;
-            DockPanel.SetDock(button, Dock.Right);
-
-            _navBarDockPanel.Children.Add(button);
-            _navBarDockPanel.Children.Add(_textBox);
-
-            _webViewDockPanel.Children.Add(_navBarDockPanel);
-            _webViewDockPanel.Children.Add(_webView2);
-
-            this.Content = _webViewDockPanel;
-            this.Title = "Compose Webmodule";
             this.Show();
         }
 
         public WebContent()
         {
             InitializeComponent();
-            SetUpAndShowWindow();
 
+            webView2.Source = new Uri(_uriString);
+
+            SetUpAndShowWindow();
         }
 
         private void ButtonGo_Click(object sender, RoutedEventArgs e)
         {
-            if (_webView2 != null && _webView2.CoreWebView2 != null)
+            if (webView2 != null && webView2.CoreWebView2 != null)
             {
-                _webView2.CoreWebView2.Navigate(_textBox.Text);
+                webView2.CoreWebView2.Navigate(addressBar.Text);
             }
         }
     }
