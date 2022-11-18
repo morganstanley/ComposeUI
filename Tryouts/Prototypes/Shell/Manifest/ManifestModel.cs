6e6c9bb687f08e8765b4c6da8943e355ccf2ad78
Maria Krupl <73655443+kruplm@users.noreply.github.com>
2022-11-18 17:40:26 +0100

    URL from manifest file

---
+++ Tryouts/Prototypes/Shell/Manifest/ManifestModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace Shell
+{
+    [Serializable]
+    internal sealed class ManifestModel
+    {
+        public string AppName { get; set; } = string.Empty; //set?
+        public string Url { get; set; }  = string.Empty; //set?
+
+
+        public static JsonSerializerOptions JsonSerializerOptions = new()
+        {
+            Converters =
+            {
+                new JsonStringEnumConverter()
+            },
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+    }
+}
+
+
+
