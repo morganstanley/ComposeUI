33a563a3e597d424c45864eef84fbd9179624511
kruplm <73655443+kruplm@users.noreply.github.com>
2023-02-07 14:08:38 +0100

    Fix runtime errors and scss issues

--- Tryouts/Plugins/ApplicationPlugins/process explorer/src/app/DTOs/ModuleInfo.ts
+++ Tryouts/Plugins/ApplicationPlugins/process explorer/src/app/DTOs/ModuleInfo.ts
@@ -1,11 +1,15 @@
-import { Byte } from "@angular/compiler/src/util";
+//TODO: Byte can't be found because of a possible breaking change. Investigate why Byte is used from here and why a DTO depends on two framework libs.
+// Check out https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Int8Array and other formats.
+
+//import { Byte } from "@angular/compiler/src/util";
 import { Guid } from "igniteui-angular-core";
 
 export class ModuleInfo{
     Version: Guid;
     Name: string;
     VersionRedirectedFrom: string;
-    PublicKeyToken: Array<Byte>;
+   // PublicKeyToken: Array<Byte>;
+    PublicKeyToken: Array<any>;
     Location: string;
     Information: Array<any>;
 }
