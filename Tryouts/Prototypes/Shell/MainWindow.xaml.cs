a6c8acd48008a80ae0a06b137408d467be62f2c1
kruplm <73655443+kruplm@users.noreply.github.com>
2023-02-20 17:55:17 +0100

    Add Ribbon menu

--- Tryouts/Prototypes/Shell/MainWindow.xaml.cs
+++ Tryouts/Prototypes/Shell/MainWindow.xaml.cs
@@ -29,13 +29,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Ribbon;
+using System.Windows.Controls.Ribbon.Primitives;
 
 namespace Shell
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         internal List<WebContent> webContentList { get; set; } = new List<WebContent>();
         private ManifestModel config;
