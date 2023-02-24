9c1d29acbea4ca8c73980836d1d463d036f8d408
kruplm <73655443+kruplm@users.noreply.github.com>
2023-02-24 13:37:00 +0100

    Add Ribbon menu (#160)
    * Add Ribbon menu
    
    * Clean-up code
    
    * Added Missing reference to Messaging.Core
    
    * Removed unnecessary assembly
    
    * Clean-up

--- Tryouts/Prototypes/Shell/MainWindow.xaml.cs
+++ Tryouts/Prototypes/Shell/MainWindow.xaml.cs
@@ -12,18 +12,31 @@
 //  * and limitations under the License.
 //  */
 
+using Manifest;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using Manifest;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Controls.Ribbon;
 
 namespace Shell
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         internal List<WebWindow> webWindows { get; set; } = new List<WebWindow>();
         private ManifestModel config;
