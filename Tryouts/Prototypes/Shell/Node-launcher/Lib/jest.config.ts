a6d3379415a3371760494b628b50b85e587b5bfe
kruplm <73655443+kruplm@users.noreply.github.com>
2023-02-24 11:10:28 +0100

    Configure jest

---
+++ Tryouts/Prototypes/Shell/Node-launcher/Lib/jest.config.ts
@@ -0,0 +1,21 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // Automatically clear mock calls, instances, contexts and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+
+  // A preset that is used as a base for Jest's configuration
+  preset: "ts-jest"
+};
