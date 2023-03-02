a2f0883d2047429bd6a631a0305f637f51210d31
kruplm <73655443+kruplm@users.noreply.github.com>
2023-03-02 12:57:06 +0100

    Include app.js

--- Tryouts/Plugins/ApplicationPlugins/defaultApp/app.js
+++ Tryouts/Plugins/ApplicationPlugins/defaultApp/app.js
@@ -1,19 +1,16 @@
 /* Morgan Stanley makes this available to you under the Apache License, Version 2.0 (the "License"). You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. See the NOTICE file distributed with this work for additional information regarding copyright ownership. Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. */
 
-//Copied from the Node-Launcher-Demo
-// import { BrowserWindow } from '@morgan-stanley/composeui-node-launcher';
+import { BrowserWindow } from '@morgan-stanley/composeui-node-launcher';
 
-//Later we would need update it to instantly if no url have been added to the composeui command
-// function windowOpenExample() {
-//     const window = new BrowserWindow(
-//         {
-//             url: "http//localhost:8080",
-//             title: "Default window",
-//             width: 1600,
-//             height: 800
-//         });
+function windowOpenExample() {
+    const window = new BrowserWindow({
+        url: "http://localhost:8080",
+        title: "ComposeUI",
+        width: 1600,
+        height: 800
+    });
 
-//     window.open();
-// }
+    window.open();
+}
 
-// windowOpenExample();
+windowOpenExample();
