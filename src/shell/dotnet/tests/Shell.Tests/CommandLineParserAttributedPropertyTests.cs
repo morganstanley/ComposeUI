0f7969a2e9681cea59b91aa8b82f31278375723d
Róbert Zöldi-Kovács <13298643+ZKRobi@users.noreply.github.com>
2023-03-11 10:31:41 +0100

    Added tests for CommandLineParser

---
+++ src/shell/dotnet/tests/Shell.Tests/CommandLineParserAttributedPropertyTests.cs
@@ -0,0 +1,49 @@
+﻿using Shell.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellTests
+{
+    public class CommandLineParserAttributedPropertyTests
+    {
+        public class AttributedOptions
+        {
+            [Display(Name = "opt", Description = "Option with overwritten name")]
+            public string RenamedOption { get; set; }
+
+            [Display(Description = "Just a description")]
+            public string Option { get; set; }
+        }
+
+        [Fact]
+        public void TestParsingRenamedOption()
+        {
+            var testValue = Guid.NewGuid().ToString();
+            var options = CommandLineParser.Parse<AttributedOptions>(new[] { "--opt", testValue });
+            Assert.NotNull(options);
+            Assert.Equal(testValue.ToString(), options.RenamedOption);
+        }
+
+        [Fact]
+        public void TestParsingRenamedOptionWithOriginalName()
+        {
+            var testValue = Guid.NewGuid().ToString();
+            var options = CommandLineParser.Parse<AttributedOptions>(new[] { "--renamedOption", testValue });
+            Assert.NotNull(options);
+            Assert.Null(options.RenamedOption);
+        }
+
+        [Fact]
+        public void TestParsingOptionWithDescription()
+        {
+            var testValue = Guid.NewGuid().ToString();
+            var options = CommandLineParser.Parse<AttributedOptions>(new[] { "--option", testValue });
+            Assert.NotNull(options);
+            Assert.Equal(testValue.ToString(), options.Option);
+        }
+    }
+}
---
+++ src/shell/dotnet/tests/Shell.Tests/CommandLineParserCustomPropertyStringTests.cs
@@ -0,0 +1,59 @@
+﻿using Shell.Utilities;
+
+namespace ShellTests
+{
+    public class CommandLineParserCustomPropertyStringTests
+    {
+        private const string Default = "default";
+        public class CustomPropertyStringOptions
+        {
+            private string? _option = null;
+            public string? Option
+            {
+                get { return _option ?? Default; }
+                set { _option = value; }
+            }
+        }
+
+        [Fact]
+        public void TestParseSimpleStringWithEmptyParameterList()
+        {
+            var options = CommandLineParser.Parse<CustomPropertyStringOptions>(new string[0]);
+            Assert.NotNull(options);
+            Assert.Equal(Default, options.Option);
+        }
+
+        [Fact]
+        public void TestParseSimpleStringWithValueProvided()
+        {
+            var testValue = Guid.NewGuid();
+            var options = CommandLineParser.Parse<CustomPropertyStringOptions>(new[] { "--option", testValue.ToString() });
+            Assert.NotNull(options);
+            Assert.Equal(testValue.ToString(), options.Option);
+        }
+
+        [Fact]
+        public void TestParseSimpleStringWithoutValue()
+        {
+            var testValue = Guid.NewGuid();
+            Assert.Throws<InvalidOperationException>(() => CommandLineParser.Parse<CustomPropertyStringOptions>(new[] { "--option" }));
+        }
+
+        [Fact]
+        public void TestParseSimpleStringWithOnlyDifferentParameter()
+        {
+            var options = CommandLineParser.Parse<CustomPropertyStringOptions>(new[] { "--stuff", "irrelevant" });
+            Assert.NotNull(options);
+            Assert.Equal(Default, options.Option);
+        }
+
+        [Fact]
+        public void TestParseSimpleStringWithOtherParameters()
+        {
+            var testValue = Guid.NewGuid();
+            var options = CommandLineParser.Parse<CustomPropertyStringOptions>(new[] { "--firstParam", "irrelevant", "--option", testValue.ToString(), "--lastParam", "irrelevant" });
+            Assert.NotNull(options);
+            Assert.Equal(testValue.ToString(), options.Option);
+        }
+    }
+}
