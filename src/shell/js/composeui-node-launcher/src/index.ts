16754c1275633387b2e6e902c346cacdb2090c22
kruplm <73655443+kruplm@users.noreply.github.com>
2023-03-14 13:18:56 +0100

    Rename BrowserWindow

--- src/shell/js/composeui-node-launcher/src/index.ts
+++ src/shell/js/composeui-node-launcher/src/index.ts
@@ -1,4 +1,4 @@
-﻿export { BrowserWindow } from './BrowserWindow.js';
+﻿export { BrowserWindow } from './browserWindow.js';
 export { Launcher } from './launcher.js';
 export { WindowConfig } from './windowConfig.js';
 export * from './cli/cli.js';
