84ee1bdc89cd852682964e6fa34456740c1494e4
kruplm <73655443+kruplm@users.noreply.github.com>
2023-03-21 17:26:15 +0100

    Fix install script

--- src/shell/js/composeui-node-launcher/src/index.ts
+++ src/shell/js/composeui-node-launcher/src/index.ts
@@ -3,4 +3,3 @@ export { Launcher } from './launcher.js';
 export { WindowConfig } from './windowConfig.js';
 export * from './cli/cli.js';
 export { executeScriptFile } from './cli/executeScrtiptFile.js';
-export * from './cli/install.js';
