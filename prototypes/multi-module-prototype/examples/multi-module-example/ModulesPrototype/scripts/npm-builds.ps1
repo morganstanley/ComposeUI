06e49148dad003d035b31d6c313faf9e4efc380b
Marton Balassa <7115274+BalassaMarton@users.noreply.github.com>
2023-03-23 11:41:43 +0100

    Moved ModulesPrototype under prototypes Removed Core/Abstractions package

--- prototypes/multi-module-prototype/examples/multi-module-example/ModulesPrototype/scripts/npm-builds.ps1
+++ prototypes/multi-module-prototype/examples/multi-module-example/ModulesPrototype/scripts/npm-builds.ps1
@@ -1,3 +1,3 @@
 # Building the MessageRouter-web-client, because without it the chart couldn't be started.
-cd $PSScriptRoot\..\..\..\..
+cd $PSScriptRoot\..\..\..\..\..\..
 npx lerna run build --scope '{@morgan-stanley/composeui-messaging-client,@morgan-stanley/composeui-example-chart}'
