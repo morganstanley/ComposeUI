a75c26bbd5da55ca8e4198636a6c4af04bdd9df7
kruplm <73655443+kruplm@users.noreply.github.com>
2023-03-29 17:06:29 +0200

    Launcher exeutes the downloaded binary

--- src/shell/js/composeui-node-launcher/src/launcher.ts
+++ src/shell/js/composeui-node-launcher/src/launcher.ts
@@ -1,7 +1,11 @@
 import { execFile } from 'child_process';
 import { WindowConfig } from './windowConfig';
+import { fileURLToPath } from "node:url";
 
 export class Launcher {
+    private composeuiBinaryFileName = process.platform === 'win32' ? 'ComposeUI-Shell.exe' : 'ComposeUI-Shell'; 
+    private composeuiBinaryFilePath = fileURLToPath(new URL(`./../dist/${this.composeuiBinaryFileName}`, import.meta.url));   
+
     private processArgs(config?: WindowConfig) {
         let argsArray = [];
         if (config) {
@@ -19,14 +23,15 @@ export class Launcher {
         if (!config?.url) {
             throw new Error("At least the url must be specified!");
         } 
-            const child = execFile("ComposeUI-Shell.exe", argsArray, (error, stdout, stderr) => {
-                console.log(stdout);
-                if (error) {
-                    throw error;
-                }
-            });
-        
-            let exithandler = function() { process.exit() };
-            child.on('close', exithandler);
+            
+        const child = execFile(this.composeuiBinaryFilePath, argsArray, (error, stdout, stderr) => {
+            console.log(stdout);
+            if (error) {
+                throw error;
+            }
+        });
+    
+        let exithandler = function() { process.exit() };
+        child.on('close', exithandler);
     }
 }
