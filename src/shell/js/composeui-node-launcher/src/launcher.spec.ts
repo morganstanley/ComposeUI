3a1c1cb0a2e8f7e4341476761095d4e7f695835e
kruplm <73655443+kruplm@users.noreply.github.com>
2023-03-30 17:19:57 +0200

    Mock Launcher

--- src/shell/js/composeui-node-launcher/src/launcher.spec.ts
+++ src/shell/js/composeui-node-launcher/src/launcher.spec.ts
@@ -1,18 +1,37 @@
+import { mocked } from 'jest-mock'
+import { jest } from '@jest/globals';
+
 import { Launcher } from './launcher';
 import { WindowConfig } from './windowConfig';
 
-describe('Launcher', () => {
-  let testWindowConfig: WindowConfig;
-  let testLauncher: Launcher;
 
-  beforeAll( () => {
-    testLauncher = new Launcher();
+jest.mock('./launcher', () => {
+  return {
+    Launcher: jest.fn().mockImplementation(() => {
+      return {
+        launch: () => {throw new Error("At least the url must be specified!")},
+      };
+    })
+  };
+});
+
+describe('Launcher', () => {
+  const MockedLauncher = mocked(Launcher, { shallow:true });
+
+  beforeEach(() => {
+     MockedLauncher.mockClear();
   });
 
-  test('launch() - "Window config has no parameters', () => {
-    testWindowConfig = {};
+  it('constructor was called', () => {
+    const testLauncher = new Launcher();
+    expect(MockedLauncher).toHaveBeenCalledTimes(1);
+  });
+
+  it('throws if Window config has no parameters', () => {
+    let testWindowConfig: WindowConfig = {};
 
     expect(() => {
+      const testLauncher = new Launcher();
       testLauncher.launch(testWindowConfig);
     }).toThrow("At least the url must be specified!");
   });
