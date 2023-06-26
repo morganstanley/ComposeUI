3418dc4e5316b9ce93d7408a079f4b09bb7d4cb7
kruplm <73655443+kruplm@users.noreply.github.com>
2023-06-26 13:30:49 +0200

    Rename ComposeDesktopAgent to ComposeUIDesktopAgent

--- src/shell/js/composeui-fdc3/src/index.ts
+++ src/shell/js/composeui-fdc3/src/index.ts
@@ -1,4 +1,4 @@
-/* 
+﻿/* 
  *  Morgan Stanley makes this available to you under the Apache License,
  *  Version 2.0 (the "License"). You may obtain a copy of the License at
  *       http://www.apache.org/licenses/LICENSE-2.0.
