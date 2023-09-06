7870baa4e7c100ead65e900bfb4ec2b05d7ec2cb
agyen <izagyen96@gmail.com>
2023-09-06 08:27:12 +0000

    Add karma config, fix failing tests

--- prototypes/process-explorer/oss-frontend/src/app/app.component.spec.ts
+++ prototypes/process-explorer/oss-frontend/src/app/app.component.spec.ts
@@ -1,12 +1,29 @@
 /* Morgan Stanley makes this available to you under the Apache License, Version 2.0 (the "License"). You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. See the NOTICE file distributed with this work for additional information regarding copyright ownership. Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. */
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatListModule } from '@angular/material/list'
+import { AppRoutingModule } from './app-routing.module';
+
+
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [
+      RouterTestingModule, 
+      MatToolbarModule, 
+      MatIconModule, 
+      MatSidenavModule, 
+      BrowserAnimationsModule,
+      MatListModule,
+      AppRoutingModule
+    ],
+    declarations: [AppComponent, SidenavComponent]
   }));
 
   it('should create the app', () => {
@@ -15,16 +32,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'Process Explorer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+    expect(app.title).toEqual('Process Explorer');
   });
 });
--- prototypes/process-explorer/oss-frontend/src/app/app.component.ts
+++ prototypes/process-explorer/oss-frontend/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'oss';
+  title = 'Process Explorer';
 }
