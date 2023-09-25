7fd5d5fbb90a421ff234c610ddc477df715143fb
Marton Balassa <7115274+BalassaMarton@users.noreply.github.com>
2023-09-25 10:58:37 +0200

    examples(fdc3):Added the fdc3-appdirectory example.

--- examples/serve-chart-and-grid.ps1
+++ examples/serve-chart-and-grid.ps1
@@ -1,4 +1,6 @@
-cd..
+Push-Location .
+Set-Location $PSScriptRoot\..
 npm i
 npx lerna run build --stream --scope "{@morgan-stanley/composeui-example-chart,@morgan-stanley/composeui-example-grid}"
 npx lerna run start --stream --scope "{@morgan-stanley/composeui-example-chart,@morgan-stanley/composeui-example-grid}"
+Pop-Location
