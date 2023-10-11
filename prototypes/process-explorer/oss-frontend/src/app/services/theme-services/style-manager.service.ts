79227ddac3db5cc7b06b01c422ba12b4dfde5bfb
agyen <izagyen96@gmail.com>
2023-10-11 10:17:54 +0000

    Refactor style-manager

--- prototypes/process-explorer/oss-frontend/src/app/services/theme-services/style-manager.service.ts
+++ prototypes/process-explorer/oss-frontend/src/app/services/theme-services/style-manager.service.ts
@@ -7,28 +7,28 @@ import { Injectable } from '@angular/core';
 export class StyleManagerService {
 
   setStyle(key: string, href: string) {
-    getLinkElementForKey(key).setAttribute("href", href);
+    this.getLinkElementForKey(key).setAttribute("href", href);
   }
-}
 
-function getLinkElementForKey(key: string) {
-  return getExistingLinkElementByKey(key) || createLinkElementWithKey(key);
-}
+  getLinkElementForKey(key: string) {
+    return this.getExistingLinkElementByKey(key) || this.createLinkElementWithKey(key);
+  }
 
-function getExistingLinkElementByKey(key: string) {
-  return document.head.querySelector(
-    `link[rel="stylesheet"].${getClassNameForKey(key)}`
-  );
-}
+  getExistingLinkElementByKey(key: string) {
+    return document.head.querySelector(
+      `link[rel="stylesheet"].${this.getClassNameForKey(key)}`
+    );
+  }
 
-function createLinkElementWithKey(key: string) {
-  const linkEl = document.createElement("link");
-  linkEl.setAttribute("rel", "stylesheet");
-  linkEl.classList.add(getClassNameForKey(key));
-  document.head.appendChild(linkEl);
-  return linkEl;
-}
+  createLinkElementWithKey(key: string) {
+    const linkEl = document.createElement("link");
+    linkEl.setAttribute("rel", "stylesheet");
+    linkEl.classList.add(this.getClassNameForKey(key));
+    document.head.appendChild(linkEl);
+    return linkEl;
+  }
 
-function getClassNameForKey(key: string) {
-  return `app-${key}`;
+  getClassNameForKey(key: string) {
+    return `app-${key}`;
+  } 
 }
