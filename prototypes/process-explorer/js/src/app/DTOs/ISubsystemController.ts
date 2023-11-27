c69e046c71d3146d288d7ad1400f27fe5bcc5c2a
kruplm <73655443+kruplm@users.noreply.github.com>
2023-11-27 10:22:47 +0100

    chore: Delete IgniteUI Frontend

--- prototypes/process-explorer/js/src/app/DTOs/ISubsystemController.ts
+++
@@ -1,8 +0,0 @@
-export interface ISubsystemController{
-    LaunchSubsystem(subsystemId: string): void;
-    LaunchSubsystems(subsystemIds: Array<string>): void;
-    RestartSubsystem(subsystemId: string): void;
-    RestartSubsystems(subsystemIds: Array<string>): void;
-    ShutdownSubsystem(subsystemId: string): void;
-    ShutdownSubsystems(subsystemIds: Array<string>): void;
-}
