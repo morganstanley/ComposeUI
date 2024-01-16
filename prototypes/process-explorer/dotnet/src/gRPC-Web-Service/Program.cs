5a98661fded19a40c0fd753ce124d90d377bb829
agyen <izagyen96@gmail.com>
2024-01-16 11:15:28 +0000

    Example grpc web with greet

---
+++ prototypes/process-explorer/dotnet/src/gRPC-Web-Service/Program.cs
@@ -0,0 +1,28 @@
+﻿using gRPC_Web_Service.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
+
+var app = builder.Build();
+
+app.UseGrpcWeb();
+app.UseCors();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>().EnableGrpcWeb()
+                            .RequireCors("AllowAll"); ;
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
