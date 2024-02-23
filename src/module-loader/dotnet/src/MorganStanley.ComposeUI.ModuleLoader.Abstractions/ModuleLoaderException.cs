819184f0ace3c17c225d6fb038134271c0c67353
Marton Balassa <7115274+BalassaMarton@users.noreply.github.com>
2024-02-23 17:39:11 +0100

    fix(fdc3):Fixed intent resolution bugs in DesktopAgent and its tests

---
+++ src/module-loader/dotnet/src/MorganStanley.ComposeUI.ModuleLoader.Abstractions/ModuleLoaderException.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace MorganStanley.ComposeUI.ModuleLoader;
+
+public class ModuleLoaderException : Exception
+{
+    public ModuleLoaderException()
+    {
+    }
+
+    protected ModuleLoaderException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    public ModuleLoaderException(string? message) : base(message)
+    {
+    }
+
+    public ModuleLoaderException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
