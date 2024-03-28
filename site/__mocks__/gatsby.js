1cfdd006d1dff91ec2ebf22d6ba57bf862d7b50b
Mimi Flynn <414934+mimiflynn@users.noreply.github.com>
2024-03-28 12:05:23 -0400

    versioned documentation

---
+++ site/__mocks__/gatsby.js
@@ -0,0 +1,34 @@
+// https://www.gatsbyjs.com/docs/how-to/testing/unit-testing/
+
+const React = require('react');
+const gatsby = jest.requireActual('gatsby');
+
+module.exports = {
+  ...gatsby,
+  graphql: jest.fn(),
+  Link: jest.fn().mockImplementation(
+    // these props are invalid for an `a` tag
+    ({
+      activeClassName,
+      activeStyle,
+      getProps,
+      innerRef,
+      partiallyActive,
+      ref,
+      replace,
+      to,
+      ...rest
+    }) =>
+      React.createElement('a', {
+        ...rest,
+        href: to,
+      })
+  ),
+  Slice: jest.fn().mockImplementation(({ alias, ...rest }) =>
+    React.createElement('div', {
+      ...rest,
+      'data-test-slice-alias': alias,
+    })
+  ),
+  useStaticQuery: jest.fn(),
+};
