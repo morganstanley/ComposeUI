c0d023ab2bad73ba2d94b190f83537a1df1285c7
Lilla Orban <Lilla.Orban@morganstanley.com>
2024-04-16 10:06:59 +0200

    fix(icon-handling) - Disposing icon after ImageSource is created from its handle

---
+++ src/shell/dotnet/Shell/Utilities/IconUtilities.cs
@@ -0,0 +1,49 @@
+﻿// /*
+//  * Morgan Stanley makes this available to you under the Apache License,
+//  * Version 2.0 (the "License"). You may obtain a copy of the License at
+//  *
+//  *      http://www.apache.org/licenses/LICENSE-2.0.
+//  *
+//  * See the NOTICE file distributed with this work for additional information
+//  * regarding copyright ownership. Unless required by applicable law or agreed
+//  * to in writing, software distributed under the License is distributed on an
+//  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+//  * or implied. See the License for the specific language governing permissions
+//  * and limitations under the License.
+//  */
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+using MorganStanley.ComposeUI.Utilities;
+
+namespace MorganStanley.ComposeUI.Shell.Utilities;
+public static class IconUtilities
+{
+    public static System.Windows.Media.ImageSource ToImageSource(this Bitmap bitmap)
+    {
+        try
+        {
+            using var memoryStream = new MemoryStream();
+
+            bitmap.Save(memoryStream, ImageFormat.Png);
+            memoryStream.Position = 0;
+
+            var bitmapImage = new BitmapImage();
+
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = memoryStream;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+            bitmapImage.Freeze();
+
+            return bitmapImage;
+        }
+        finally
+        {
+            NativeMethods.DeleteObject(bitmap.GetHbitmap());
+        }
+    }
+}
+
