21aad95e07086ca6c196bea5237494437682f6ee
agyen <izagyen96@gmail.com>
2024-06-03 09:45:42 +0000

    New Grpc web service initial

---
+++ prototypes/process-explorer/dotnet/src/gRPC-Web-Service/Program.cs
@@ -0,0 +1,48 @@
+﻿using gRPC_Web_Service.DependencyInjection;
+using gRPC_Web_Service.Server.GrpcServer;
+using gRPC_Web_Service.Server.Abstractions;
+using gRPC_Web_Service.Server.Infrastructure.Grpc;
+using gRPC_Web_Service.Services;
+using gRPC_Web_Service.Sever.Abstractions;
+using MorganStanley.ComposeUI.ProcessExplorer.Core.DependencyInjection;
+using MorganStanley.ComposeUI.ProcessExplorer.Abstractions.Infrastructure;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
+builder.Services.AddProcessExplorerAggregator();
+builder.Services.AddProcessMonitorWindows();
+builder.Services.AddSubsystemController();
+builder.Services.AddSingleton<IUiHandler, GrpcUiHandler>();
+builder.Services.Configure<ProcessExplorerServerOptions>(op =>
+{
+    op.Port = 5060;
+    op.MainProcessId = Process.GetCurrentProcess().Id;
+    op.EnableProcessExplorer = true;
+});
+builder.Services.AddSingleton<GrpcListenerService>();
+builder.Services.AddSingleton<IHostedService>(provider => provider.GetRequiredService<GrpcListenerService>());
+builder.Services.AddSingleton<ProcessExplorerServer>(provider => provider.GetRequiredService<GrpcListenerService>());
+
+var app = builder.Build();
+app.UseGrpcWeb();
+app.UseCors();
+// Configure the HTTP request pipeline.
+//app.MapGrpcService<GreeterService>().EnableGrpcWeb().RequireCors("AllowAll");
+app.MapGrpcService<ProcessExplorerMessageHandlerService>().EnableGrpcWeb().RequireCors("AllowAll");
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ prototypes/process-explorer/dotnet/src/gRPC-Web-Service/Server/MessageHandler.cs
@@ -0,0 +1,120 @@
+﻿// Morgan Stanley makes this available to you under the Apache License,
+// Version 2.0 (the "License"). You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0.
+// 
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership. Unless required by applicable law or agreed
+// to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+using gRPC_Web_Service.Extensions;
+using gRPC_Web_Service.Logging;
+using Microsoft.Extensions.Logging;
+using MorganStanley.ComposeUI.ProcessExplorer.Abstractions;
+using ProcessExplorer.Abstractions.Infrastructure.Protos;
+
+namespace gRPC_Web_Service.Server;
+
+internal static class MessageHandler
+{
+    public static async void HandleIncomingGrpcMessages(
+        Message message,
+        IProcessInfoAggregator processInfoAggregator,
+        ILogger? logger = null)
+    {
+        try
+        {
+            var ids = message.Subsystems.Select(subsystem => subsystem.Key);
+
+            switch (message.Action)
+            {
+                case ActionType.TerminateSubsystemsAction:
+                    await processInfoAggregator.SubsystemController.ShutdownSubsystems(ids);
+
+                    break;
+
+                case ActionType.RestartSubsystemsAction:
+                    await processInfoAggregator.SubsystemController.RestartSubsystems(ids);
+
+                    break;
+
+                case ActionType.LaunchSubsystemsAction:
+                    await processInfoAggregator.SubsystemController.LaunchSubsystems(ids);
+
+                    break;
+
+                case ActionType.LaunchSubsystemsWithDelayAction:
+                    try
+                    {
+                        if (ids.ElementAt(0) == null) break;
+
+                        var id = Guid.Parse(ids.ElementAt(0)); // or foreach? 
+                        await processInfoAggregator.SubsystemController.LaunchSubsystemAfterTime(id, message.PeriodOfDelay);
+                    }
+                    catch (Exception exception)
+                    {
+                        logger?.GrpcMessageReadingError(exception, exception);
+                    }
+
+                    break;
+
+                case ActionType.AddRuntimeInfoAction:
+                    await processInfoAggregator.AddRuntimeInformation(
+                        message.AssemblyId,
+                        message.RuntimeInfo.DeriveProcessInfoCollectorData());
+
+                    break;
+
+                case ActionType.AddConnectionListAction:
+                    await processInfoAggregator.AddConnectionCollection(
+                        message.AssemblyId,
+                        message.Connections.Select(connection => connection.DeriveConnectionInfo()));
+
+                    break;
+
+                case ActionType.UpdateConnectionAction:
+                    await processInfoAggregator.UpdateOrAddConnectionInfo(
+                        message.AssemblyId,
+                        message.Connections.First().DeriveConnectionInfo());
+
+                    break;
+
+                case ActionType.UpdateEnvironmentVariablesAction:
+                    await processInfoAggregator.UpdateOrAddEnvironmentVariablesInfo(
+                        message.AssemblyId,
+                        message.EnvironmentVariables);
+
+                    break;
+
+                case ActionType.UpdateRegistrationsAction:
+                    await processInfoAggregator.UpdateRegistrations(
+                        message.AssemblyId,
+                        message.Registrations.Select(registration => registration.DeriveRegistration()));
+
+                    break;
+
+                case ActionType.UpdateModulesAction:
+                    await processInfoAggregator.UpdateOrAddModuleInfo(
+                        message.AssemblyId,
+                        message.Modules.Select(module => module.DeriveModule()));
+
+                    break;
+
+                case ActionType.UpdateConnectionStatusAction:
+                    await processInfoAggregator.UpdateConnectionStatus(
+                        message.AssemblyId,
+                        message.ConnectionStatusChanges.First().Key,
+                        message.ConnectionStatusChanges.First().Value);
+
+                    break;
+            }
+        }
+        catch (Exception exception)
+        {
+            logger?.GrpcMessageHandlingError(exception, exception);
+        }
+    }
+}
