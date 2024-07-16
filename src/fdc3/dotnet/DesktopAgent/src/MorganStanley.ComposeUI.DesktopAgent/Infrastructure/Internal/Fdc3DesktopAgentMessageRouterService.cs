decd36c8fc0fb9d4798dca7e40ad3d9065c78fcc
Robert Zoldi-Kovacs <13298643+ZKRobi@users.noreply.github.com>
2024-07-16 10:45:06 +0200

    Add JsonStringEnumConverter to the Fdc3DesktopAgentMessageRouterService JSON configuration

--- src/fdc3/dotnet/DesktopAgent/src/MorganStanley.ComposeUI.DesktopAgent/Infrastructure/Internal/Fdc3DesktopAgentMessageRouterService.cs
+++ src/fdc3/dotnet/DesktopAgent/src/MorganStanley.ComposeUI.DesktopAgent/Infrastructure/Internal/Fdc3DesktopAgentMessageRouterService.cs
@@ -45,6 +45,7 @@ internal class Fdc3DesktopAgentMessageRouterService : IHostedService
             new IconJsonConverter(),
             new ImageJsonConverter(),
             new IntentMetadataJsonConverter(),
+            new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
         }
     };
 
