d2852103aa6f3ec926189979b90282f4360a9f76
kruplm <73655443+kruplm@users.noreply.github.com>
2024-10-24 16:42:47 +0200

    chore: Create Rollup bug repro example

---
+++ chart.js
@@ -0,0 +1,38 @@
+﻿﻿import Chart from 'highcharts/es-modules/Core/Chart/Chart.js';
+import ColumnSeries from 'highcharts/es-modules/Series/Column/ColumnSeries.js';
+import * as Highcharts from 'highcharts';
+
+let chart;
+
+
+window.addEventListener('load', function () {
+  chart = Highcharts.chart('container', {
+    chart: {
+      type: 'column',
+      events: {
+        load: requestData
+      }
+    },
+    title: {
+      text: 'Monthly Sales Data'
+    },
+    yAxis: {
+      minPadding: 0.2,
+      maxPadding: 0.2,
+      title: {
+        text: 'Value',
+        margin: 80,
+      }
+    },
+    xAxis: {
+      categories: ['Jan','Feb','March','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+    },
+    series: [{
+      name: 'Buy',
+      data: [] 
+    }, {
+      name: 'Sell',
+      data: []
+    }]
+  });
+});
---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+
+
+export default {
+    input: 'chart.js',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'iife',
+        name: 'chart'
+    },
+    plugins: [
+        resolve(),
+        commonjs()
+    ]
+};
