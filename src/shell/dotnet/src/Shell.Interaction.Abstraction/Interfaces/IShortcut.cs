42baa206bae5da091040aef96f452fde24408a8b
Pahan, Szabolcs <188366465+phn-ms@users.noreply.github.com>
2025-03-25 07:30:14 +0100

    Add IShortcut interface for managing keyboard shortcuts
    Introduce IShortcut interface in MorganStanley.ComposeUI.Shell.Interaction.Abstraction.Interfaces namespace. This interface includes methods for registering, checking, and unregistering keyboard shortcuts for specified applications. XML documentation comments are provided for each method.

---
+++ src/shell/dotnet/src/Shell.Interaction.Abstraction/Interfaces/IShortcut.cs
@@ -0,0 +1,35 @@
+﻿namespace MorganStanley.ComposeUI.Shell.Interaction.Abstraction.Interfaces;
+
+/// <summary>
+/// Defines the interface for managing keyboard shortcuts.
+/// </summary>
+public interface IShortcut
+{
+    /// <summary>
+    /// Registers a keyboard shortcut for the specified application.
+    /// </summary>
+    /// <param name="appId">The ID of the application.</param>
+    /// <param name="shortcut">The keyboard shortcut to register.</param>
+    /// <param name="callback">The callback to invoke when the shortcut is triggered.</param>
+    void Register(string appId, string shortcut, object callback);
+
+    /// <summary>
+    /// Checks if a keyboard shortcut is registered for the specified application.
+    /// </summary>
+    /// <param name="appId">The ID of the application.</param>
+    /// <param name="shortcut">The keyboard shortcut to check.</param>
+    void IsRegistered(string appId, string shortcut);
+
+    /// <summary>
+    /// Unregisters a keyboard shortcut for the specified application.
+    /// </summary>
+    /// <param name="appId">The ID of the application.</param>
+    /// <param name="shortcut">The keyboard shortcut to unregister.</param>
+    void Unregister(string appId, string shortcut);
+
+    /// <summary>
+    /// Unregisters all keyboard shortcuts for the specified application.
+    /// </summary>
+    /// <param name="appId">The ID of the application.</param>
+    void UnregisterAll(string appId);
+}
