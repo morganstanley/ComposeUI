ce7d66acbc1c1db9d59b00515202c2dcf2b7d044
kruplm <73655443+kruplm@users.noreply.github.com>
2025-07-28 13:13:14 +0200

    chore(examples/fdc3-chart): Move build to webpack (#1054)

--- examples/fdc3-chart-and-grid/js-chart/chart.js
+++ examples/fdc3-chart-and-grid/js-chart/chart.js
@@ -1,6 +1,16 @@
-﻿import Chart from 'highcharts/es-modules/Core/Chart/Chart.js';
-import ColumnSeries from 'highcharts/es-modules/Series/Column/ColumnSeries.js';
-import NoDataToDisplay from 'highcharts/modules/no-data-to-display';
+﻿// Morgan Stanley makes this available to you under the Apache License,
+// Version 2.0 (the "License"). You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0.
+// 
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership. Unless required by applicable law or agreed
+// to in writing, software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
+import NoDataToDisplay from 'highcharts/modules/no-data-to-display.js';
 import * as Highcharts from 'highcharts';
 import { ContextTypes } from '@finos/fdc3';
 import { Intents } from '@finos/fdc3';
