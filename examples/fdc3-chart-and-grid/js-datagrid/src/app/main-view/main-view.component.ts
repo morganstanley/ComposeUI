6d903f3bfd4fe48fee4c2be69f7f59ca70f114cb
Mimi Flynn <414934+mimiflynn@users.noreply.github.com>
2025-12-22 10:33:40 -0500

    ng update to 19

--- examples/fdc3-chart-and-grid/js-datagrid/src/app/main-view/main-view.component.ts
+++ examples/fdc3-chart-and-grid/js-datagrid/src/app/main-view/main-view.component.ts
@@ -3,9 +3,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main-view',
-  templateUrl: './main-view.component.html',
-  styleUrls: ['./main-view.component.scss']
+    selector: 'app-main-view',
+    templateUrl: './main-view.component.html',
+    styleUrls: ['./main-view.component.scss'],
+    standalone: false
 })
 export class MainViewComponent {
 
